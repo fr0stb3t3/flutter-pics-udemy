3863fb487f271543c6d9d125a0f96c88eebd62b3
Meherwan <msgil4@student.monash.edu>
2020-07-22 03:17:52 +1000

    styled it

--- lib/src/widgets/ImageList.dart
+++ lib/src/widgets/ImageList.dart
@@ -6,12 +6,40 @@ class ImageList extends StatelessWidget {
 
   ImageList(this.images);
 
+  Widget paddedImage(ImageJsonModel rawImage) {
+    return Padding(
+      child: Image.network(rawImage.url),
+      padding: EdgeInsets.only(bottom: 10.0),
+    );
+  }
+
+  Widget imageAndTitleColumn(ImageJsonModel image) {
+    return Column(
+      children: <Widget>[paddedImage(image), Text(image.title)],
+    );
+  }
+
+  Widget buildImageBuilder(ImageJsonModel image) {
+    return Container(
+      // child: Image.network(image.url),
+      child: imageAndTitleColumn(image),
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.purple[300],
+          width: 5.0,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Image.network(images[index].url);
+        return buildImageBuilder(images[index]);
       },
     );
   }
